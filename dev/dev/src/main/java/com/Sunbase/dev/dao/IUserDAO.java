9d8fa349a831666b63f9114ab612075c2fb7766c
Harshal12j <98580498+Harshal12j@users.noreply.github.com>
2024-02-03 16:24:27 +0530

    Delete dev/dev directory

--- dev/dev/src/main/java/com/Sunbase/dev/dao/IUserDAO.java
+++
@@ -1,7 +0,0 @@
-package com.Sunbase.dev.dao;
-import com.Sunbase.dev.repository.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface IUserDAO extends JpaRepository<User, Long>{
-    public User findByUsername(String username);
-
-}
